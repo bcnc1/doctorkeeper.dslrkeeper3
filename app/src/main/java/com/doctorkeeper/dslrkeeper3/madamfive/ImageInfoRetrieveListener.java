393d10ecd4a18b4407ffb385e493fa2d86568f04
jy Kim <jy.kim@doctorkeeper.com>
2022-01-28 14:26:16 +0900

    initial

---
+++ app/src/main/java/com/doctorkeeper/dslrkeeper3/madamfive/ImageInfoRetrieveListener.java
@@ -0,0 +1,17 @@
+package com.doctorkeeper.dslrkeeper3.madamfive;
+
+import android.graphics.Bitmap;
+
+import com.doctorkeeper.dslrkeeper3.ptp.Camera;
+import com.doctorkeeper.dslrkeeper3.ptp.model.ObjectInfo;
+
+/**
+ * Created by thinoo on 6/30/17.
+ */
+
+public class ImageInfoRetrieveListener implements Camera.RetrieveImageInfoListener{
+    @Override
+    public void onImageInfoRetrieved(int objectHandle, ObjectInfo objectInfo, Bitmap thumbnail) {
+
+    }
+}
